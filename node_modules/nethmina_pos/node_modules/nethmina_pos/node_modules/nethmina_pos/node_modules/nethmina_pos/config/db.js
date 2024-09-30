b1d2a6038637601e2a86a06d68387a72e4371d97
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-09-30 09:52:39 +0530

    start project

---
+++ node_modules/nethmina_pos/node_modules/nethmina_pos/node_modules/nethmina_pos/node_modules/nethmina_pos/config/db.js
@@ -0,0 +1,35 @@
+
+const mysql = require("mysql2/promise");
+require('dotenv').config();
+
+const db = mysql.createPool({
+  host: "nethmina-pos.c744e6yi8s7w.eu-north-1.rds.amazonaws.com",
+  user: "root",
+  database: "nethmina_POS",
+  password: "Nethmina123#",
+  port:"3306",
+  // timezone: 'Z'
+});
+
+// Function to test database connection
+async function testConnection() {
+  try {
+    const connection = await db.getConnection(); // Get a connection from the pool
+    console.log("Connected to Database!");
+    connection.release(); // Release the connection back to the pool
+  } catch (err) {
+    console.error("Failed to connect to the database:", err.message);
+  }
+}
+
+// Call the function to test the connection
+testConnection();
+
+module.exports = db;
+
+//Queries
+
+/*
+
+
+ */
