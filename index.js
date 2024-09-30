170d3c2805ae52f8420103c2841e4da12068395f
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-09-30 16:04:38 +0530

    develop store API and functions

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const database = require("./config/db");
+
+const salesRoutes = require("./routes/sales");
+const storesRoutes = require("./routes/stores");
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/sales", salesRoutes);
+app.use("/stores", storesRoutes);
+
 
 
 app.listen(3000, () => {
