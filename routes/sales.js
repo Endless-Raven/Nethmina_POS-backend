cef438565c864997db798bd9b9264d84fd28958b
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-10-01 16:52:45 +0530

    Develop sales API and controller

--- routes/sales.js
+++ routes/sales.js
@@ -1,12 +1,15 @@
 const express = require("express");
 
-const { makesale} = require("../controllers/sales");
+const { makesale ,getsales , getsalebyid} = require("../controllers/sales");
 
 const router = express.Router();
 
 
 
 router.post("/",makesale );
+router.get("/",getsales );
+router.get("/:sale_id",getsalebyid );
+
 
 module.exports = router;
 
