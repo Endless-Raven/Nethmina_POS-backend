8e0117bd3664291a13f7149de61d3c1dce06c73d
ShaznaSalman <shaznasalman10@gmail.com>
2024-10-08 10:21:28 +0530

    merge issue fixed

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,8 @@
 const express = require("express");
 
-const { makesale ,getsales , getsalebyid ,getDailySalesReport , getSalesItemsByDate} = require("../controllers/sales");
+const { makesale ,getsales , getsalebyid ,getDailySalesReport} = require("../controllers/sales");
 
 const router = express.Router();
-
-
 
 
 router.post("/",makesale );
@@ -12,7 +10,6 @@ router.get("/",getsales );
 router.get("/getsalebyid/:sale_id",getsalebyid );
 router.get("/daily-sales-report",getDailySalesReport );
 
-router.get("/getSalesItems/ByDate",getSalesItemsByDate ); //get sele details by date
 
 module.exports = router;
 
