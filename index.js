52f5dba72e85831b9b48c552256e7661771a7d60
ShaznaSalman <shaznasalman10@gmail.com>
2024-10-10 20:23:32 +0530

    hosting test 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const database = require("./config/db");
-
+const path = require('path');
 const fs = require('fs')
 
 const file = fs.readFileSync('./392E2CF1BDBE028C1CDB993D4B3EF153.txt')
@@ -21,10 +21,15 @@ app.use(cors());
 app.use(express.json());
 
 // Root route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Nethmina POS backend!'); // Welcome message
-  // res.sendFile('./392E2CF1BDBE028C1CDB993D4B3EF153.txt');
-});
+const filePath = path.resolve(__dirname, '392E2CF1BDBE028C1CDB993D4B3EF153.txt');
+
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get('/.well-known/pki-validation/392E2CF1BDBE028C1CDB993D4B3EF153.txt', (req, res) => {
+  res.sendFile(filePath);
+})
 
 app.use("/sales", salesRoutes);
 app.use("/product", productRoutes);
