a95bec41452a4955cb0df9fc641d0d1229963735
ShaznaSalman <shaznasalman10@gmail.com>
2024-10-10 10:41:19 +0530

    added env for database

--- config/db.js
+++ config/db.js
@@ -3,12 +3,11 @@ const mysql = require("mysql2/promise");
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host:"nethminapos-main.crem6m0k23gc.eu-north-1.rds.amazonaws.com",
-  user: "admin",
-  database: "nethmina_POS",
-  password: "nethmina123main",
-  port:"3306",
-  // timezone: 'Z'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Function to test database connection
@@ -27,6 +26,7 @@ testConnection();
 
 module.exports = db;
 
+
 //Queries
 
 /*
@@ -42,13 +42,12 @@ CREATE TABLE `cashiers` (
   PRIMARY KEY (`cashier_id`),
   KEY `fk_store_id` (`store_id`),
   CONSTRAINT `fk_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`store_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+);
 
 CREATE TABLE `customers` (
   `customer_id` int NOT NULL AUTO_INCREMENT,
   `customer_name` varchar(100) NOT NULL,
-  `customer_email` varchar(100) NOT NULL,
+  `customer_email` varchar(100) DEFAULT NULL,
   `customer_phone_number` varchar(20) DEFAULT NULL,
   `customer_address` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -56,26 +55,24 @@ CREATE TABLE `customers` (
   PRIMARY KEY (`customer_id`),
   UNIQUE KEY `email` (`customer_email`),
   UNIQUE KEY `customer_email` (`customer_email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+);
 
 CREATE TABLE `products` (
   `product_id` int NOT NULL AUTO_INCREMENT,
   `product_name` varchar(255) NOT NULL,
   `product_price` decimal(10,2) DEFAULT NULL,
-  `warranty_period` int DEFAULT '12',
+  `warranty_period` varchar(45) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `product_stock` decimal(10,2) DEFAULT NULL,
   `product_type` varchar(100) NOT NULL,
   `brand_name` varchar(100) NOT NULL,
   `product_model` varchar(100) NOT NULL,
-  `imei_number` varchar(50) DEFAULT NULL,
+  `imei_number` longtext,
   `product_wholesale_price` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`product_id`),
   UNIQUE KEY `product_name_UNIQUE` (`product_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+);
 
 CREATE TABLE `sales` (
   `sale_id` varchar(45) NOT NULL,
@@ -88,11 +85,10 @@ CREATE TABLE `sales` (
   PRIMARY KEY (`sale_id`),
   KEY `cashier_id` (`cashier_id`),
   CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`cashier_id`) REFERENCES `cashiers` (`cashier_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+);
 
 CREATE TABLE `sales_items` (
-  `sale_item_id` varchar(45) NOT NULL,
+  `sale_item_id` int NOT NULL AUTO_INCREMENT,
   `sale_id` varchar(45) NOT NULL,
   `product_id` int NOT NULL,
   `item_quantity` varchar(100) DEFAULT NULL,
@@ -101,54 +97,14 @@ CREATE TABLE `sales_items` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `imei_number` varchar(50) DEFAULT NULL,
   `discount` decimal(5,2) DEFAULT '0.00',
+  `warranty_period` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`sale_item_id`),
-  UNIQUE KEY `imei_number_UNIQUE` (`imei_number`),
   KEY `sale_id` (`sale_id`),
   KEY `product_id` (`product_id`),
-  CONSTRAINT `sales_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `stores` (
-  `store_id` int NOT NULL AUTO_INCREMENT,
-  `store_name` varchar(100) NOT NULL,
-  `store_address` varchar(100) DEFAULT NULL,
-  `store_phone_number` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`store_id`),
-  UNIQUE KEY `unique_store_name` (`store_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('admin','cashier','manager') NOT NULL,
-  `store_id` int DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `warranties` (
-  `warranty_id` int NOT NULL AUTO_INCREMENT,
-  `product_id` int NOT NULL,
-  `sale_id` varchar(45) NOT NULL,
-  `warranty_start_date` date NOT NULL,
-  `warranty_end_date` date NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `period` varchar(45) NOT NULL,
-  PRIMARY KEY (`warranty_id`),
-  KEY `product_id` (`product_id`),
-  KEY `sale_id` (`sale_id`),
-  CONSTRAINT `warranties_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+  KEY `sales_items_ibfk_3_idx` (`warranty_period`),
+  CONSTRAINT `sales_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+  CONSTRAINT `sales_items_ibfk_3` FOREIGN KEY (`warranty_period`) REFERENCES `products` (`product_name`)
+);
 
 CREATE TABLE `stock` (
   `stockid` int NOT NULL AUTO_INCREMENT,
@@ -163,6 +119,44 @@ CREATE TABLE `stock` (
   KEY `fk_stock_store_name` (`store_name`),
   CONSTRAINT `fk_stock_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
   CONSTRAINT `fk_stock_store_name` FOREIGN KEY (`store_name`) REFERENCES `stores` (`store_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
+
+CREATE TABLE `stores` (
+  `store_id` int NOT NULL AUTO_INCREMENT,
+  `store_name` varchar(100) NOT NULL,
+  `store_address` varchar(100) DEFAULT NULL,
+  `store_phone_number` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`store_id`),
+  UNIQUE KEY `unique_store_name` (`store_name`)
+);
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','cashier','manager') NOT NULL,
+  `store_id` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+);
+
+CREATE TABLE `warranties` (
+  `warranty_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `sale_id` varchar(45) NOT NULL,
+  `warranty_start_date` date NOT NULL,
+  `warranty_end_date` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `warranty_period` varchar(45) NOT NULL,
+  PRIMARY KEY (`warranty_id`),
+  KEY `product_id` (`product_id`),
+  KEY `sale_id` (`sale_id`),
+  CONSTRAINT `warranties_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
+);
 
  */
