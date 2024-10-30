0df5907282110e0f9a1fea55db2d3db1ed4b5f50
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-10-30 10:53:04 +0530

    fix

--- routes/stores.js
+++ routes/stores.js
@@ -15,7 +15,7 @@ router.put("/:store_name",updatestorebyname); // update store by name
 router.get("/getstorename/:store_id",getstorenamebyid); 
 
 router.get("/getstore/names",getstorenames); // get store names for inventory
-router.put('/updateStore/:store_id', updateStoreById); // Use store_id as a path parameter
+router.put('/updateStore', updateStoreById); // Use store_id as a path parameter
 router.delete('/deleteStore/:store_id', deleteStore); //delete store
 
 module.exports = router;
