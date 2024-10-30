287d3daac378d91b0e709e6faf364c768bcd9607
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-10-30 11:57:25 +0530

    fix bugs cashire names functions for bills and store and users, store functions

--- routes/stores.js
+++ routes/stores.js
@@ -15,7 +15,7 @@ router.put("/:store_name",updatestorebyname); // update store by name
 router.get("/getstorename/:store_id",getstorenamebyid); 
 
 router.get("/getstore/names",getstorenames); // get store names for inventory
-router.put('/updateStore', updateStoreById); // Use store_id as a path parameter
+router.put('/update/Storebid', updateStoreById); // Use store_id as a path parameter
 router.delete('/deleteStore/:store_id', deleteStore); //delete store
 
 module.exports = router;
